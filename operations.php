82802b3f13b6a988656b093304a425a2f9f1008e
Mílton <mirtu1370@gmail.com>
2019-04-02 21:54:22 +0100

    operations

---
+++ operations.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>goodbye</title>
+ </head>
+ <body>
+ <?php 
+ 
+  $x= 10;
+  $y= 7;
+
+  echo($x + $y); 
+  echo($x - $y);
+  echo($x * $y);
+  echo($x / $y);
+ 
+ ?> 
+ </body>
+</html>
